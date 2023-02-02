00588d4ea07e41728e4d93e9371401dd2e724ec9
fthrslntgy <fatiharslantugay@gmail.com>
2023-02-02 23:04:34 +0300

    Try sign up with credentials in Test class

--- Assets/Scripts/Login/FirebaseAuthHandler.cs
+++ Assets/Scripts/Login/FirebaseAuthHandler.cs
@@ -1,27 +1,17 @@
 ﻿using Proyecto26;
 using UnityEngine;
 
-/// <summary>
-/// Handles authentication calls to Firebase
-/// </summary>
-
 public static class FirebaseAuthHandler
 {
-    private const string ApiKey = "AIzaSyCpIcpAABw5swuRMiCBcZ1coZe9fpyqy3M"; //TODO: Change [API_KEY] to your API_KEY
+    private const string ApiKey = "AIzaSyCpIcpAABw5swuRMiCBcZ1coZe9fpyqy3M";
 
-    /// <summary>
-    /// Signs in a user with their Id Token
-    /// </summary>
-    /// <param name="token"> Id Token </param>
-    /// <param name="providerId"> Provider Id </param>
     public static void SingInWithToken(string token, string providerId)
     {
         var payLoad =
             $"{{\"postBody\":\"id_token={token}&providerId={providerId}\",\"requestUri\":\"http://localhost\",\"returnIdpCredential\":true,\"returnSecureToken\":true}}";
-        RestClient.Post($"https://identitytoolkit.googleapis.com/v1/accounts:signInWithIdp?key={ApiKey}", payLoad).Then(
+        RestClient.Post($"https://identitytoolkit.googleapis.com/v1/accounts:signUpWithIdp?key={ApiKey}", payLoad).Then(
             response =>
             {
-                // You now have the userId (localId) and the idToken of the user!
                 Debug.Log(response.Text);
             }).Catch(Debug.Log);    
     }
