4a12c7b18de89e5e872652fcd4e300de9decb800
mrtcngnn <mrtcngonen@gmail.com>
2023-04-02 03:41:01 +0300

    object menu is created for apartment-1 scene

---
+++ Assets/Scripts/Object Menu/ObjectMenuButtons.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement; 
+
+public class ObjectMenuButtons : MonoBehaviour {
+
+    public void Button1Trigger(){
+        Debug.Log("Button 1 is triggered!");
+    }
+
+    public void Button2Trigger(){
+        Debug.Log("Button 2 is triggered!");
+    }
+
+    public void Button3Trigger(){
+        Debug.Log("Button 3 is triggered!");
+    }
+
+    public void Button4Trigger(){
+        Debug.Log("Button 4 is triggered!");
+    }
+
+    public void Button5Trigger(){
+        Debug.Log("Button 5 is triggered!");
+    }
+
+    public void Button6Trigger(){
+        Debug.Log("Button 6 is triggered!");
+    }
+
+    public void Button7Trigger(){
+        Debug.Log("Button 7 is triggered!");
+    }
+
+    public void Button8Trigger(){
+        Debug.Log("Button 8 is triggered!");
+    }
+
+}
