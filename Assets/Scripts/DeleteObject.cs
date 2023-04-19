6c39ff2f51f1d0b793417533e0f3406bf554be83
abugraokkali <abugraokkali@outlook.com>
2023-04-20 00:53:21 +0300

    fix: multiple object delete bug

--- Assets/Scripts/DeleteObject.cs
+++ Assets/Scripts/DeleteObject.cs
@@ -5,29 +5,43 @@ using UnityEngine.UI;
 
 public class DeleteObject : MonoBehaviour
 {
-    public Button yourButton;
     public GameObject modal;
-    public GameObject prefab;
+    private GameObject objectToDelete;
 
     // Start is called before the first frame update
     void Start()
     {
-        Button btn = yourButton.GetComponent<Button>();
+        Button btn = modal.transform.Find("Button").gameObject.GetComponent<Button>();
         btn.onClick.AddListener(TaskOnClick);
     }
 
     // Update is called once per frame
     void TaskOnClick()
     {
-        Destroy(prefab);
+        Destroy(objectToDelete);
         modal.SetActive(false);
     }
 
     public void hoverEnter()
     {
+        //modal.SetActive(true);
+
+
+        // Get object bounds
+        Bounds objectBounds = gameObject.GetComponent<Renderer>().bounds;
+
+        // Calculate modal position
+        Vector3 modalPosition = new Vector3(objectBounds.center.x, objectBounds.max.y -0.1f, objectBounds.center.z);
+
+        // Set modal position
+        modal.transform.position = modalPosition;
+
+        // Show modal
         modal.SetActive(true);
-        modal.transform.position = gameObject.transform.position;
-        modal.transform.position = new Vector3(modal.transform.position.x, modal.transform.position.y + 1, modal.transform.position.z);
+
+        //modal.transform.position = gameObject.transform.position;
+        //modal.transform.position = new Vector3(modal.transform.position.x, modal.transform.position.y + 1, modal.transform.position.z);
+        objectToDelete = this.gameObject;
     }
 
     public void hoverExit()
