5583715dc096e1f94180f5f8aca2a945b2b5acff
fthrslntgy <fatiharslantugay@gmail.com>
2023-05-09 01:54:53 +0300

    Add static maptile

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System;
+
+public class Map : MonoBehaviour
+{
+    private string apiKey = "AIzaSyD-F9iw7B4zZo21WOuXFiyzFsldtDSFtKU";
+    public float lat = 39.921852f;
+    public float lon = 32.799187f;
+    public int zoom = 12;
+    public enum resolution { low = 1, high = 2 };
+    public resolution mapResolution = resolution.low;
+    public enum type { roadmap, satellite, gybrid, terrain };
+    public type mapType = type.roadmap;
+    private string url = "";
+    private int mapWidth = 640;
+    private int mapHeight = 640;
+    private bool mapIsLoading = false; //not used. Can be used to know that the map is loading 
+    private Rect rect;
+
+    private string apiKeyLast;
+    private float latLast = 39.921852f;
+    private float lonLast = 32.799187f;
+    private int zoomLast = 12;
+    private resolution mapResolutionLast = resolution.low;
+    private type mapTypeLast = type.roadmap;
+    private bool updateMap = true;
+
+
+    void Start() {
+        StartCoroutine(GetGoogleMap());
+        rect = gameObject.GetComponent<RawImage>().rectTransform.rect;
+        mapWidth = (int)Math.Round(rect.width);
+        mapHeight = (int)Math.Round(rect.height);
+    }
+
+    void Update() {
+        if (updateMap && (apiKeyLast != apiKey || !Mathf.Approximately(latLast, lat) || !Mathf.Approximately(lonLast, lon) || zoomLast != zoom || mapResolutionLast != mapResolution || mapTypeLast != mapType)) {
+            rect = gameObject.GetComponent<RawImage>().rectTransform.rect;
+            mapWidth = (int)Math.Round(rect.width);
+            mapHeight = (int)Math.Round(rect.height);
+            StartCoroutine(GetGoogleMap());
+            updateMap = false;
+        }
+    }
+
+    IEnumerator GetGoogleMap() {
+        float mark1lat = lat + 0.05f; 
+        float mark1lon = lon + 0.05f; 
+        url = "https://maps.googleapis.com/maps/api/staticmap?center=39.92185,32.79919&zoom=12&size=800x600&scale=high&maptype=roadmap&markers=label:1|39.86185,32.84919&markers=label:2|39.9685,32.75019&markers=label:3|39.9085,32.82019&key=AIzaSyD-F9iw7B4zZo21WOuXFiyzFsldtDSFtKU";
+        mapIsLoading = true;
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture(url);
+        yield return www.SendWebRequest();
+        if (www.result != UnityWebRequest.Result.Success) {
+            Debug.Log("WWW ERROR: " + www.error);
+        } else {
+            mapIsLoading = false;
+            gameObject.GetComponent<RawImage>().texture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+
+            apiKeyLast = apiKey;
+            latLast = lat;
+            lonLast = lon;
+            zoomLast = zoom;
+            mapResolutionLast = mapResolution;
+            mapTypeLast = mapType;
+            updateMap = true;
+        }
+    }
+}
